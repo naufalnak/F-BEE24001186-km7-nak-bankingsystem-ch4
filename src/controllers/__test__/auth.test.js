a508cdcf4d22a08ef53ede19b80e0f54c24ee62d
naufalnak <naufal.ndak17@gmail.com>
2024-11-08 14:37:31 +0700

    fix auth test

--- src/controllers/__test__/auth.test.js
+++ src/controllers/__test__/auth.test.js
@@ -64,7 +64,7 @@ describe("Auth Controller", () => {
     expect(jwt.sign).toHaveBeenCalledWith(
       { userId: mockUser.user_id },
       process.env.JWT_SECRET,
-      { expiresIn: "1h" }
+      { expiresIn: process.env.JWT_EXPIRES_IN }
     );
   });
 
